1781075fabeea52965c398b350e0f047e4504709
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-02-28 01:42:57 +0100

    anchoranalysis/anchor-assembly#3 introduced auto-population of Experiment Identifiers; significantly refactored experiment identifiers

--- addplugins/anchor-launcher/src/main/java/org/anchoranalysis/launcher/executor/selectparam/task/UpdateTaskName.java
+++ addplugins/anchor-launcher/src/main/java/org/anchoranalysis/launcher/executor/selectparam/task/UpdateTaskName.java
@@ -28,45 +28,40 @@ package org.anchoranalysis.launcher.executor.selectparam.task;
 
 import java.nio.file.Path;
 
-import org.anchoranalysis.launcher.CommandLineException;
+import org.anchoranalysis.experiment.ExperimentExecutionArguments;
+import org.anchoranalysis.experiment.ExperimentExecutionException;
 import org.anchoranalysis.launcher.executor.selectparam.SelectParam;
-import org.anchoranalysis.launcher.executor.selectparam.path.CustomManagerFromPath;
-import org.anchoranalysis.launcher.executor.selectparam.path.PathConverter;
 
 /**
- * SelectParam<Path> factory for tasks
+ * Updates task-name AND delegates to another SelectParam<Path>
  * 
  * @author owen
  *
  */
-public class TaskFactory {
+class UpdateTaskName extends SelectParam<Path> {
 
-	/** If the argument a name (no extension, no root, no special-chars apart from forward-slashes), then construct an automatic path to the tasks
-	 *  in the configuration directory. Otherwise treat as path to BeanXML */
-	public static SelectParam<Path> pathOrTaskName( String[] args, Path configDir ) {
-    	
-		if (args.length!=1) {
-			throw new CommandLineException("One and only one argument is permitted after -t");
-		}
-		
-		String taskArg = args[0];
-		
-		if (isTaskName(taskArg)) {
-			return new UpdateTaskName(
-				new CustomManagerFromPath( constructPathForTaskName(taskArg, configDir) ),
-				taskArg
-			);
-		} else {
-			return new CustomManagerFromPath( PathConverter.pathFromArg(taskArg) );
-		}
+	private SelectParam<Path> delegate;
+	private String taskName;
+
+	public UpdateTaskName(SelectParam<Path> delegate, String taskName) {
+		super();
+		this.delegate = delegate;
+		this.taskName = taskName;
 	}
-	
-	private static Path constructPathForTaskName( String arg, Path configDir ) {
-		return configDir.resolve("tasks").resolve(arg + ".xml");
+
+	@Override
+	public Path select(ExperimentExecutionArguments eea) throws ExperimentExecutionException {
+		eea.setTaskName(taskName);
+		return delegate.select(eea);
 	}
-	
-	// Check if it contains only a restricted set of characters... alphaNumeric, hyphen, underscore, forward-slash
-	private static boolean isTaskName( String arg ) {
-		return arg.matches("^[a-zA-Z0-9_\\-\\/]+$");
+
+	@Override
+	public boolean isDefault() {
+		return delegate.isDefault();
+	}
+
+	@Override
+	public String describe() throws ExperimentExecutionException {
+		return delegate.describe();
 	}
 }
