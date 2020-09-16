1c800ceeb8ca989dd482651ac2967c47a81ac517
Owen Feehan <owenfeehan@users.noreply.github.com>
2020-09-16 20:10:55 +0200

    sonarqube

--- addplugins/anchor-launcher/src/main/java/org/anchoranalysis/launcher/executor/ExperimentExecutor.java
+++ addplugins/anchor-launcher/src/main/java/org/anchoranalysis/launcher/executor/ExperimentExecutor.java
@@ -96,8 +96,8 @@ public class ExperimentExecutor {
             // Special behaviour if everything has defaults
             logger.logFormatted(
                     "%s.%nLearn how to select inputs, outputs and tasks with 'anchor -%s'.%n",
-                    defaultBehaviourString.get(),
-                    ParseArgumentsAndRunExperiment.OPTION_HELP); // NOSONAR
+                    defaultBehaviourString.get(),   // NOSONAR
+                    ParseArgumentsAndRunExperiment.OPTION_HELP);
         }
 
         Experiment experimentLoaded = loadExperimentFromPath(execArgs);
