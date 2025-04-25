1ef947c5a01962dafbfe776ed42dd46dd3c2744e
Owen Feehan <owenfeehan@users.noreply.github.com>
2025-04-25 15:48:33 +0200

    Migrating anchor to use Java 24 and to 1.0.0-SNAPSHOT

--- addplugins/anchor-launcher/src/test/java/org/anchoranalysis/launcher/executor/ExperimentReaderErrorReportingTest.java
+++ addplugins/anchor-launcher/src/test/java/org/anchoranalysis/launcher/executor/ExperimentReaderErrorReportingTest.java
@@ -43,11 +43,11 @@ import org.junit.jupiter.api.function.Executable;
  */
 class ExperimentReaderErrorReportingTest {
 
-    private static TestLoader tl = TestLoader.createFromMavenWorkingDirectory();
+    private static TestLoader loader = TestLoader.createFromMavenWorkingDirectory();
 
     @BeforeAll
     static void setUp() throws Exception {
-        ExperimentExecutorAfter.initializeIfNecessary(tl.getRoot(), false, false);
+        ExperimentExecutorAfter.initializeIfNecessary(loader.getRoot(), false, false);
     }
 
     @Test
@@ -162,7 +162,7 @@ class ExperimentReaderErrorReportingTest {
             throws ExperimentExecutionException {
 
         String testPath = testPathToConfig(configName);
-        Path experimentConfigFile = tl.resolveTestPath(testPath);
+        Path experimentConfigFile = loader.resolveTestPath(testPath);
 
         // Catch and display the output
         try {
