b39ce8a0c35e2410c6e811454708f60b43ae4a83
Owen Feehan <owenfeehan@users.noreply.github.com>
2025-05-25 10:45:10 +0200

    spotless

--- src/test/java/org/anchoranalysis/assembly/MontageTest.java
+++ src/test/java/org/anchoranalysis/assembly/MontageTest.java
@@ -8,12 +8,12 @@
  * it under the terms of the GNU General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public
  * License along with this program.  If not, see
  * <http://www.gnu.org/licenses/gpl-3.0.html>.
@@ -21,20 +21,18 @@
  */
 package org.anchoranalysis.assembly;
 
-import org.junit.jupiter.api.*;
-import org.junit.jupiter.api.io.TempDir;
-
 import java.io.*;
 import java.nio.file.*;
 import java.util.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.io.TempDir;
 
-/**
- * Tests the <b>montage</b> task in an Anchor distribution.
- */
+/** Tests the <b>montage</b> task in an Anchor distribution. */
 class MontageTest {
 
     @Test
     void testTask(@TempDir Path tempDirectory) throws IOException, InterruptedException {
-    	ExecutorFixture.runAndVerify("montage", List.of(), tempDirectory, List.of("logExperiment.txt", "labelled.png"));
+        ExecutorFixture.runAndVerify(
+                "montage", List.of(), tempDirectory, List.of("logExperiment.txt", "labelled.png"));
     }
 }
