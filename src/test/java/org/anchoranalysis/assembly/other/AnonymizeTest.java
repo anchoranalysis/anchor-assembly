10b0c277b2c1023332d0af2ec12b7d338ba72733
Owen Feehan <owenfeehan@users.noreply.github.com>
2025-05-27 19:10:56 +0200

    Selected version 1.0.2 and appropriate dependencies

---
+++ src/test/java/org/anchoranalysis/assembly/other/AnonymizeTest.java
@@ -0,0 +1,49 @@
+/*-
+ * #%L
+ * Anchor Distribution
+ * %%
+ * Copyright (C) 2010 - 2025 Owen Feehan, ETH Zurich, University of Zurich, Hoffmann-La Roche
+ * %%
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public
+ * License along with this program.  If not, see
+ * <http://www.gnu.org/licenses/gpl-3.0.html>.
+ * #L%
+ */
+package org.anchoranalysis.assembly.other;
+
+import java.util.*;
+import org.anchoranalysis.assembly.TaskTestBase;
+
+/** Tests the <b>anonymize</b> task in an Anchor distribution. */
+class AnonymizeTest extends TaskTestBase {
+
+    @Override
+    protected String taskName() {
+        return "anonymize";
+    }
+
+    @Override
+    protected List<String> expectedFiles() {
+        return Arrays.asList();
+    }
+
+    @Override
+    protected boolean consoleOnly() {
+        return false;
+    }
+
+    @Override
+    protected boolean identicalSizes() {
+        return false;
+    }
+}
